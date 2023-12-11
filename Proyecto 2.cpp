06dabf8ee7971dff5c2c9a752fd55533c0c5fb0e
VillafuerTech <rvillafuerte.tech@gmail.com>
2023-12-10 22:05:28 -0500

    Código finalizado, falta ultima parte

--- Proyecto 2.cpp
+++ Proyecto 2.cpp
@@ -5,38 +5,37 @@
 #include <ctime>
 #include <random>
 #include <algorithm>
-using namespace std;
+#include <fstream>
 
-random_device rd;
-mt19937 g(rd());
+
 class Album {
 public:
-   Album(int numAlbums) {
+    Album(int numAlbums) {
         totalAlbums = numAlbums;
-        totalPacks = totalAlbums * 5; // Initialize totalPacks here
+        totalPacks = totalAlbums * packsPerAlbum;
         totalStickers = numAlbums * stickersPerAlbum;
         GenerateStickers();
         GeneratePacks();
     }
-    
+
     void GenerateStickers() {
-    for (int i = 0; i < totalAlbums; ++i) {
-        for (int j = 1; j <= stickersPerAlbum; ++j) {
-            stickers.push_back(j);
+        for (int i = 0; i < totalAlbums; ++i) {
+            for (int j = 1; j <= stickersPerAlbum; ++j) {
+                stickers.push_back(j);
+            }
         }
+        std::random_device rd;
+        std::mt19937 g(time(0));
+        std::shuffle(stickers.begin(), stickers.end(), g);
     }
-    std::random_device rd;
-    std::mt19937 g(time(0));
-    std::shuffle(stickers.begin(), stickers.end(), g);
-}
-    
+
     void GeneratePacks() {
         for (int i = 0; i < totalPacks; ++i) {
             std::vector<int> pack;
             for (int j = 0; j < stickersPerPack; ++j) {
                 pack.push_back(stickers[i * stickersPerPack + j]);
             }
-            packs.push_back(pack);
+            packs.push_back({ pack, false }); // Agrega el sobre como no abierto
         }
     }
 
@@ -46,7 +45,7 @@ public:
             return;
         }
 
-        std::vector<int> pack = packs[packNumber - 1];
+        std::vector<int>& pack = packs[packNumber - 1].first;
 
         std::cout << "Contenido del sobre " << packNumber << ": ";
         for (int i = 0; i < stickersPerPack; ++i) {
@@ -54,6 +53,8 @@ public:
             AddToAlbum(pack[i]);
         }
         std::cout << std::endl;
+
+        packs[packNumber - 1].second = true; // Marca el sobre como abierto
     }
 
     void AddToAlbum(int sticker) {
@@ -65,7 +66,7 @@ public:
         }
     }
 
-    void ShowAlbum() {
+    void ShowAlbum() const {
         std::cout << "Album:" << std::endl;
         for (int i = 1; i <= 25; ++i) {
             if (album.count(i) > 0) {
@@ -78,14 +79,14 @@ public:
         std::cout << std::endl;
     }
 
-    void ShowDuplicates() {
+    void ShowDuplicates() const {
         std::cout << "Cromos repetidos:" << std::endl;
         for (const auto& entry : duplicates) {
             std::cout << "Cromo " << entry.first << ": " << entry.second << " repetidos." << std::endl;
         }
     }
 
-    void ShowMissingStickers() {
+    void ShowMissingStickers() const {
         std::cout << "Cromos faltantes:" << std::endl;
         for (int i = 1; i <= 25; ++i) {
             if (album.count(i) == 0) {
@@ -95,23 +96,54 @@ public:
         std::cout << std::endl;
     }
 
-    void Finish() {
+    void ShowOpenedPacks() const {
+        std::cout << "Sobres abiertos:" << std::endl;
+        for (int i = 0; i < totalPacks; ++i) {
+            if (packs[i].second) {
+                std::cout << "Sobre " << i + 1 << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    void ShowRemainingPacks() const {
+        std::cout << "Sobres disponibles:" << std::endl;
+        for (int i = 0; i < totalPacks; ++i) {
+            if (!packs[i].second) {
+                std::cout << "Sobre " << i + 1 << " " << std::endl;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    void Finish() const {
         std::cout << "Album completado!" << std::endl;
         ShowDuplicates();
     }
 
-    int getTotalPacks(){
+    int getTotalPacks() const {
         return totalPacks;
     }
+
+    bool isAlbumComplete() {
+    for (int i = 1; i <= 25; ++i) {
+        if (album.count(i) == 0) {
+            return false; // If any sticker is missing, the album is not complete
+        }
+    }
+    return true; // If all stickers are present, the album is complete
+}
+
 private:
     static const int stickersPerAlbum = 25;
-    int totalPacks = totalAlbums*5;
+    int totalPacks = totalAlbums * packsPerAlbum;
     static const int stickersPerPack = 5;
-
+    static const int packsPerAlbum = 5;
+    
     int totalAlbums;
     int totalStickers;
     std::vector<int> stickers;
-    std::vector<std::vector<int>> packs;
+    std::vector<std::pair<std::vector<int>, bool>> packs; // Cambio a par para incluir estado de apertura
     std::map<int, bool> album;
     std::map<int, int> duplicates;
 };
@@ -128,21 +160,24 @@ int main() {
         std::cout << "\nOpciones:\n";
         std::cout << "1. Mostrar sobres disponibles.\n";
         std::cout << "2. Abrir sobre.\n";
-        std::cout << "3. Mostrar �lbum.\n";
+        std::cout << "3. Mostrar Album.\n";
         std::cout << "4. Mostrar cromos repetidos.\n";
         std::cout << "5. Mostrar cromos faltantes.\n";
-        std::cout << "6. Terminar.\n";
-        std::cout << "7. Reiniciar.\n";
-        std::cout << "Seleccione una opci�n: ";
+        std::cout << "6. Mostrar sobres abiertos.\n";
+        std::cout << "7. Mostrar sobres disponibles.\n";
+        std::cout << "8. Terminar.\n";
+        std::cout << "9. Reiniciar.\n";
+        std::cout << "Seleccione una opcion: ";
         std::cin >> option;
 
+        
         switch (option) {
         case 1:
-            std::cout << "Sobres disponibles: " << album.getTotalPacks() << std::endl;
+            album.ShowRemainingPacks();
             break;
         case 2:
             int packNumber;
-            std::cout << "Ingrese el n�mero del sobre a abrir: ";
+            std::cout << "Ingrese el numero del sobre a abrir: ";
             std::cin >> packNumber;
             album.OpenPack(packNumber);
             break;
@@ -156,17 +191,29 @@ int main() {
             album.ShowMissingStickers();
             break;
         case 6:
-            album.Finish();
+            album.ShowOpenedPacks();
             break;
         case 7:
+            album.ShowRemainingPacks();
+            break;
+        case 8:
+            album.Finish();
+            break;
+        case 9:
             std::cout << "Reiniciando datos..." << std::endl;
             album = Album(numAlbums);
             break;
         default:
-            std::cout << "Opci�n no v�lida." << std::endl;
+            std::cout << "Opcion no valida." << std::endl;
             break;
         }
-    } while (option != 6);
+    if (album.isAlbumComplete()) {
+        std::cout << "¡El álbum está completo!" << std::endl;
+        album.ShowDuplicates();
+        album.ShowRemainingPacks();
+        break; // Termina el bucle, terminando así el programa
+    }
+    } while (option != 8);
 
     return 0;
 }
